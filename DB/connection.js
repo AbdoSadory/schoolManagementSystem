71bfa86d56e09114e279ec784d000dd994f57f4d
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2024-05-01 18:39:08 +0300

    Database Host Connection was added and connected successfully

--- DB/connection.js
+++ DB/connection.js
@@ -1,15 +1,24 @@
+import { config } from 'dotenv'
 import { Sequelize } from 'sequelize'
 
+config()
 export const sql_config = new Sequelize(
-  'schoolmanagementsystemdb',
-  'root',
-  '123456',
+  process.env.DB_name,
+  process.env.DB_username,
+  process.env.DB_password,
   {
-    host: 'localhost',
-    dialect: 'mysql',
+    host: process.env.DB_host,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+    port: parseInt(process.env.DB_port),
   }
 )
-
+// dpg-cop3vi0l5elc739cb8vg-a
+// postgres://schoolmanagementsystemdb_user:1YhQYutIkrNCAdZFijGuIfMaL1o8tyG2@dpg-cop3vi0l5elc739cb8vg-a.oregon-postgres.render.com/schoolmanagementsystemdb
 export const connectDB = async () => {
   await sql_config
     .sync({ alter: true })
